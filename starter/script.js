6233424fc119e578bbf860b8dca017997ca1297d
bert cuyugan <subs.cuyugan@gmail.com>
2025-04-08 22:56:59 +1000

    reset the game, new game

--- starter/script.js
+++ starter/script.js
@@ -90,3 +90,23 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+btnNew.addEventListener('click', function () {
+  // Reset scores
+  scores[0] = 0;
+  scores[1] = 0;
+  currentScore = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+});
+// btnRoll.disabled = false;
